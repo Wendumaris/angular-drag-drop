159335fe923b27cecc216aedd5be39e3bd27ad28
wendumaris@gmail.com <wendumaris@gmail.com>
2019-04-30 16:54:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-drag-drop';
+
+  todos = [
+    {
+      name: 'Angular',
+      category: 'Web Development'
+    },
+    {
+      name: 'Node JS',
+      category: 'Web Development'
+    },
+    {
+      name: 'iOS',
+      category: 'App Development'
+    },
+    {
+      name: 'Java',
+      category: 'Software development'
+    }
+  ];
+  
+  completed = [
+    {
+      name: 'Android',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'MongoDB',
+      category: 'Databases'
+    },
+    {
+      name: 'Python',
+      category: 'Software Development'
+    },
+    {
+      name: 'React',
+      category: 'Web Development'
+    }
+  ];
+  
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex, event.currentIndex);
+    }
+  }
 }
+
